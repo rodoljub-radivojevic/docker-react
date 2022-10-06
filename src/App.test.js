9769597766a276b32cd5613d8f55effe12bb43bb
rodoljub-radivojevic <rodoljub.radivojevic@gmail.com>
2022-10-06 14:40:55 +0200

    Another travis.yml fix

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,12 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
 
 
 
